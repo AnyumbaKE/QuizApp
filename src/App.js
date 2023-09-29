74d6d8291cce3c4e3556fc6cda1a6902d17c132b
Stanley Anyumba <124333266+AnyumbaKE@users.noreply.github.com>
2023-09-30 01:21:43 +0300

    app.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Quiz from './Quiz.js'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Quiz />
+    )
+  }
+}
+
+export default App
